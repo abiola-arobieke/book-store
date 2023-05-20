490df6a9070393fd93083e67c5260b9d2ebf28f6
abiola <abiola.arobieke@gmail.com>
2023-05-20 06:48:23 -0400

    Modified Book.js file

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,43 +1,37 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
+// import { useState } from 'react';
+import BookList from './BookList';
 
 function Books() {
-  return (
-    <div className="d-flex book-info container">
-      <div className="book-detail w-50c">
-        <div>
-          <div className="book-category">Action</div>
-          <div className="book-title">Hunger Games</div>
-          <div className="author text-blue">Susan Spat</div>
-        </div>
-        <div className="d-flex book-feedback w-50c">
-          <button type="button" className="text-blue btn-action line">
-            Comment
-          </button>
-          <button type="button" className="text-blue btn-action line pd-l10">
-            Remove
-          </button>
-          <button type="button" className="text-blue btn-action line pd-l10">
-            Edit
-          </button>
-        </div>
-      </div>
-      <div className="d-flex progress w-50c">
-        <div className="d-flex w-50c progress-bar">
-          <FontAwesomeIcon icon={faCircleNotch} />
-          <div>
-            <div className="rating">64%</div>
-            <div>Completed</div>
-          </div>
-        </div>
-        <div className="w-50c progress-info">
-          <div className="progress-stat">Current</div>
-          <div className="chapter">Chapter 17</div>
-          <button className="btn-progress" type="button">Update progress</button>
-        </div>
-      </div>
-    </div>
-  );
+  const books = [
+    {
+      id: 1,
+      bookType: 'Action',
+      title: 'The serial killer',
+      author: 'Oyinkan Braithwaite',
+      progress: 75,
+      status: 'reading',
+      page: 'chapter 18',
+    },
+    {
+      id: 2,
+      bookType: 'Drama',
+      title: 'Things fall apart',
+      author: 'Chinua Achebe',
+      progress: 63,
+      status: 'completed',
+      page: 'chapter 10',
+    },
+    {
+      id: 3,
+      bookType: 'Play-wright',
+      title: 'Welcome to lagos',
+      author: 'Chibundu Onuzo',
+      progress: 14,
+      status: 'reading',
+      page: 'page 12',
+    },
+  ];
+  return <BookList booksProp={books} />;
 }
 
 export default Books;
