2d7394755117d7a10acfd665710d551e021811e8
abiola <abiola.arobieke@gmail.com>
2023-05-20 12:29:42 -0400

    Modify Books.js file

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,10 +1,12 @@
+import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
 import BookList from './BookList';
+import InputBooks from './InputBook';
 
 function Books() {
   const [books, setBook] = useState([
     {
-      id: 1,
+      id: '1',
       category: 'Action',
       title: 'The serial killer',
       author: 'Oyinkan Braithwaite',
@@ -13,7 +15,7 @@ function Books() {
       page: 'chapter 18',
     },
     {
-      id: 2,
+      id: '2',
       category: 'Drama',
       title: 'Things fall apart',
       author: 'Chinua Achebe',
@@ -22,7 +24,7 @@ function Books() {
       page: 'chapter 10',
     },
     {
-      id: 3,
+      id: '3',
       category: 'Play-wright',
       title: 'Welcome to lagos',
       author: 'Chibundu Onuzo',
@@ -35,7 +37,25 @@ function Books() {
   const handleRemove = (id) => {
     setBook([...books.filter((book) => book.id !== id)]);
   };
-  return <BookList booksProp={books} removeBook={handleRemove} />;
+
+  const addBook = (title, category) => {
+    const newBook = {
+      id: uuidv4(4),
+      category,
+      title,
+      author: 'John Doe',
+      progress: 25,
+      status: 'completed',
+      page: 'chapter 1',
+    };
+    setBook([...books, newBook]);
+  };
+  return (
+    <div className="bg-grey-light">
+      <BookList booksProp={books} removeBook={handleRemove} />
+      <InputBooks addNewBook={addBook} />
+    </div>
+  );
 }
 
 export default Books;
