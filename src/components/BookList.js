d60fdcbe00db98d2f82e00f68035d0b9ec06f0cd
abiola <abiola.arobieke@gmail.com>
2023-05-20 07:44:26 -0400

    Update BookList.js file

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 import BookItem from './BookItem';
 
-function BookList({ booksProp }) {
+function BookList({ booksProp, removeBook }) {
   return (
     <>
       {booksProp.map((book) => (
-        <BookItem key={book.id} book={book} />
+        <BookItem key={book.id} book={book} removeBook={() => removeBook(book.id)} />
       ))}
     </>
   );
@@ -15,6 +15,7 @@ BookList.propTypes = {
   booksProp: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number,
+      category: PropTypes.string,
       title: PropTypes.string,
       author: PropTypes.string,
       progress: PropTypes.number,
@@ -22,6 +23,7 @@ BookList.propTypes = {
       page: PropTypes.string,
     }),
   ).isRequired,
+  removeBook: PropTypes.func.isRequired,
 };
 
 export default BookList;
