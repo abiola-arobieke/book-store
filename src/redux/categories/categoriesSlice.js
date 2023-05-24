b6759d4ad0f7c2431d3a3f75a9a8adbc25ef8fb2
abiola <abiola.arobieke@gmail.com>
2023-05-24 01:46:11 -0400

    Update categoriesSlide.js file

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,12 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  status: 'Under Construction',
+  categories: [],
 };
 
 const categoriesSlice = createSlice({
   name: 'categories',
   initialState,
+  reducers: {
+    checkStatus: (state, action) => {
+      state.categories = action.payload === 'Under construction'
+        ? 'Under construction'
+        : state.categories;
+    },
+  },
 });
+
+export const { checkStatus } = categoriesSlice.actions;
 
 export default categoriesSlice.reducer;
