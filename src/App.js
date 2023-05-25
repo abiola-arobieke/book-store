32d6bd4b9f440ee20fe76df3d982b05bfddf98ba
abiola <abiola.arobieke@gmail.com>
2023-05-25 16:56:05 -0400

    Modify App.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Categories from './components/Categories';
 import Books from './components/Books';
 import Navbar from './components/Navbar';
+import { getBooksItem } from './redux/books/booksSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooksItem());
+  }, [dispatch]);
+
   return (
     <div className="co">
       <Navbar />
