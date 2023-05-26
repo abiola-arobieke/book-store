da4b27afa7377e0cdb24a75a80e121e1f4578f85
abiola <abiola.arobieke@gmail.com>
2023-05-26 15:43:11 -0400

    Update Navbar.js file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,18 +4,18 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   return (
-    <nav className="navigation">
+    <nav className="navigation bg-white">
       <div className="navbar md-6 d-flex">
         <div className="d-flex align-center w-100c">
-          <Link to="/" className="logo">
+          <Link to="/" className="logo txt-primary">
             Bookstore CMS
           </Link>
           <div className="d-flex spc-btw nav-link">
             <ul className="d-flex">
-              <Link to="/" className="nav-item">
+              <Link to="/" className="nav-item txt-upper txt-dark">
                 Books
               </Link>
-              <Link to="/categories" className="nav-item">
+              <Link to="/categories" className="nav-item txt-upper txt-dark">
                 Categories
               </Link>
             </ul>
