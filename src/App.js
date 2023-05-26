8849c73994ac8015a68ae34cee7a254956857a21
abiola <abiola.arobieke@gmail.com>
2023-05-26 15:46:58 -0400

    Add CSS styling to App.js file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import Categories from './components/Categories';
 import Books from './components/Books';
-import Navbar from './components/Navbar';
 import { getBooksItem } from './redux/books/booksSlice';
 
 function App() {
@@ -13,8 +12,7 @@ function App() {
   }, [dispatch]);
 
   return (
-    <div className="co">
-      <Navbar />
+    <div className="app">
       <Routes>
         <Route path="/" element={<Books />} />
         <Route path="/categories" element={<Categories />} />
