0b115ee4280707b678afa1043de9a2f72c799b22
abiola <abiola.arobieke@gmail.com>
2023-05-26 15:44:11 -0400

    Update Books.js file

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,9 +1,11 @@
 import BookList from './BookList';
 import InputBooks from './InputBook';
+import Navbar from './Navbar';
 
 function Books() {
   return (
-    <div className="bg-grey-light">
+    <div className="bg-grey-light pb-3">
+      <Navbar />
       <BookList />
       <InputBooks />
     </div>
